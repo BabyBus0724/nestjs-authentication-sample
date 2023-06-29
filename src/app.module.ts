d7a865cf2347c79edb196424e9f9bef98d37eb45
popstar724 <135359534+popstar724@users.noreply.github.com>
2023-06-29 10:01:37 -0400

    MongoDB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    //MongoDB Connection
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory:async (configService:ConfigService) => configService.getMongoConfig(),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.service.ts
@@ -0,0 +1,29 @@
+import * as dotenv from 'dotenv';
+
+export class ConfigService {
+    private readonly envConfig: Record<string, string>;
+    constructor() {
+        const result = dotenv.config();
+        if (result.error) {
+            this.envConfig = process.env;
+        } else {
+            this.envConfig = result.parsed;
+        }
+    }
+
+    public get(key: string): string {
+        return this.envConfig[key];
+    }
+
+    public async getPortConfig(){
+        return process.env.PORT || 3000
+    }
+
+    public async getMongoConfig() {
+        return {
+            uri : 'mongodb://localhost:27017/course',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    }
+} 
